5749f6695727d47b37b30a44077b4aabe13a7f72
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-26 08:31:45 -0400

    useState-object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'John',
+    age: 24,
+    message: 'random message',
+  });
+  const changeMessage = () => {
+    // use ...separate operator in useState hook prevent other parameter being wipe out in object
+    setPerson({ ...person, message: 'hello world' });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
