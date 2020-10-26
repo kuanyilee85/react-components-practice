33978c1602b5e59e5a90c55ba8066a7fa7193b24
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-26 08:40:32 -0400

    many small state

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,15 +6,23 @@ const UseStateObject = () => {
     age: 24,
     message: 'random message',
   });
+
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('random, message');
+
   const changeMessage = () => {
-    // use ...separate operator in useState hook prevent other parameter being wipe out in object
-    setPerson({ ...person, message: 'hello world' });
+    // use ...separate operator in useState hook preventing other parameter being wipe out in object
+    // setPerson({ ...person, message: 'hello world' });
+
+    // you can also set many small state rather than a object contain many property
+    setMessage('hello world');
   };
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className="btn" onClick={changeMessage}>
         change message
       </button>
