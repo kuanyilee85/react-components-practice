64b258dbcd249e205bbfaf91130a579b5940d4bd
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-26 23:09:38 -0400

    useEffect: dependency list

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -4,11 +4,21 @@ import React, { useState, useEffect } from 'react';
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
+  // this one will run when value update: [value]
   useEffect(() => {
+    console.log('call useEffect');
     if (value > 1) {
       document.title = `new message(${value})`;
     }
-  });
+  }, [value]);
+
+  // this one will only run at initial render: empty []
+  useEffect(() => {
+    console.log('hello world');
+  }, []);
+
+  console.log('render component');
   return (
     <>
       <h1>{value}</h1>
