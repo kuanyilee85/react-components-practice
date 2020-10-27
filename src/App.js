78d447601890a2502c72ccac4589c4074ca03fc0
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-26 23:41:39 -0400

    useEffect: fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
-import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
-// import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+// import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 
 function App() {
   return (
